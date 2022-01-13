e1e950d92c0f858f075d9fbdad4dc03a66cbc90d
fichince <acchoi@gmail.com>
2022-01-13 14:58:39 -0500

    Responsive navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,10 +5,6 @@ import MD from './common/MD';
 
 const NAV = [
   {
-    name: 'Home',
-    to: '/'
-  },
-  {
     name: 'Blog',
     to: '/blog'
   },
@@ -18,7 +14,19 @@ const NAV = [
   },
 ];
 
-const Layout = ({ pageTitle, children }) => {
+const NavLink = (props) => {
+  const { to, name } = props;
+  return (
+    <Link to={to}>
+      <div className="flex-no-grow flex-no-shrink relative p-3 leading-normal text-tertiary font-display 
+        no-underline flex items-center hover:bg-tertiary hover:text-accent">
+          {name}
+      </div>
+    </Link>
+  );
+};
+
+const Layout = ({ pageTitle, children, ...rest }) => {
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -27,7 +35,21 @@ const Layout = ({ pageTitle, children }) => {
         }
       }
     }
-  `)
+  `);
+  const { site: { siteMetadata: { title } } } = data;
+
+  const [ expanded, setExpanded ] = React.useState(window.innerWidth > 640);
+  React.useEffect(() => {
+    const resizeHandler = () => {
+      setExpanded(window.innerWidth > 640);
+    };
+
+    window.addEventListener('resize', resizeHandler);
+
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+    };
+  }, []);
 
   return (
     <div className="m-auto">
@@ -35,25 +57,32 @@ const Layout = ({ pageTitle, children }) => {
         <body className="bg-gradient-to-br from-primary to-secondary min-h-screen" /> 
       </Helmet>
 
-      <div className="flex justify-between align-baseline
-          bg-black text-white p-3">
-        <header className="text-lg">{data.site.siteMetadata.title}</header>
-        <nav>
-          <ul className="flex">
+      <nav className="relative select-none bg-accent sm:flex sm:items-stretch w-full text-lg sm:text-xl">
+        <div className="flex flex-no-shrink items-stretch h-12">
+          <NavLink to="/" name={title} />
+          <button className="block sm:hidden cursor-pointer ml-auto relative w-12 h-12 p-4" onClick={() => setExpanded(!expanded)}>
+            { expanded ?
+              <svg className="fill-current text-tertiary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>
+              :
+              <svg className="fill-current text-tertiary" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            }
+          </button>
+        </div>
+        { expanded &&
+          <div className="sm:flex sm:items-stretch sm:flex-no-shrink sm:flex-grow">
+            <div className="sm:flex sm:items-stretch sm:justify-end ml-auto">
             { NAV.map((n, i) => (
-              <li index={i} className="ml-5">
-                <Link to={n.to}>
-                  {n.name}
-                </Link>
-              </li>
+              <NavLink to={n.to} name={n.name} />
             ))}
-          </ul>
-        </nav>
-      </div>
-      <main className="p-5 pb-20">
+            </div>
+          </div>
+        }
+      </nav>
+
+      <main className="pt-10 pb-20">
         { pageTitle &&
         <>
-          <div className="mt-3 sm:mt-7 mx-auto text-3xl sm:text-5xl text-center text-accent font-display">
+          <div className="mt-3 sm:mt-5 mx-auto text-3xl sm:text-5xl text-center text-accent font-display">
             <MD md={pageTitle} />
           </div>
           <hr className="my-4 mx-auto w-full sm:w-3/4 text-center border-accent" />
