6c91cbcf43eb87429acc12532acf53ce0cb72699
fichince <acchoi@gmail.com>
2022-01-27 13:46:06 -0500

    Fixing key prop

--- src/components/common/Tags.jsx
+++ src/components/common/Tags.jsx
@@ -5,8 +5,8 @@ const Tags = ({ tags }) => {
   return (
     <div className="flex">
       { tags.map((tag) => (
-        <div className="mr-1 last:mr-0">
-          <Link key={tag} to={`/blog/tag/${tag}`}>
+        <div className="mr-1 last:mr-0" key={tag}>
+          <Link to={`/blog/tag/${tag}`}>
             <div className="py-0.5 px-1 text-xs 
               border-accent text-accent
               hover:bg-accent hover:text-gray-300
